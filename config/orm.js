b3cec76f776d60258f1d52017ac09794a3f878fa
Will Palafox <wpalafox@gmail.com>
2016-12-14 21:26:35 -0600

    orm and model created

---
+++ config/orm.js
@@ -0,0 +1,111 @@
+var connection = require("./connection.js");
+
+
+// Question Mark function that adheres to SQL syntax
+function printQuestionMarks(num){
+	var arr = [];
+
+	for(var i =0; i < num; i++){
+		arr.push("?");
+
+	}
+
+	return arr.toString();
+
+
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    
+    if (Object.hasOwnProperty.call(ob, key)) {
+      
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+
+
+
+
+
+
+
+
+var orm = {  
+
+		selectAll: function(tableInput, cb) {
+			var queryString = "SELECT * FROM "+ tableInput + ";";
+			connection.query(queryString, function(err, res){
+				if(err) {
+					throw err;
+				}
+				cb(res);
+				
+			});  
+			
+
+
+			},
+			//vals is an array of value we will save to cols
+			// cols are where we will insert the vals 
+
+		insertOne: function (table, cols, vals, cb){
+			var queryString = "INSERT INTO " + table;
+
+			queryString += " (";
+			queryString += cols.toString();
+			queryString += ") ";
+			queryString +='VALUES (';
+			queryString += printQuestionMarks(vals.length);
+			queryString += ") ";
+
+			console.log(queryString); 
+
+			connection.query()
+
+			connection.query(queryString, vals, function(err, res) {
+		      if (err) {
+		        throw err;
+		      }
+		      cb(res);
+		    });
+
+
+		},
+
+		//An example of objColVals {name: dog, hungry: true}
+		updateOne: function(table, objColVal, condition, cb){
+			var queryString = "UPDATE " + table;
+
+			queryString += " SET ";
+			queryString += objToSql(objColVals);
+			queryString += " Where ";
+			queryString += condition;
+
+			console.log(queryString);
+			connection.query(queryString, function(err, result){
+				if (err) {
+					throw err;
+				}
+
+
+				cb(result);
+
+			});
+		}, 
+
+
+
+};
+
+
+
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,43 @@
+//Import the orm we built so we can create a function that will talk to the mySQL database
+var orm = require("../config/orm.js");
+
+
+var burger = {
+	selectAll: function(cb){
+	  orm.selectall("burgerz", function(res){
+	  	cb(res);
+	  });
+
+	},  
+	//The variables cols and vals are arrays
+	insertOne: function(cols, vals, cb){
+		orm.insertOne('burgerz', cols, vals, function(res){
+			cb(res);
+
+		});
+
+	},
+
+	updateOne: function(objColVal, condition, cb){
+		orm.updateOne('burgerz', objColVal, condition, function (res){
+
+			cb(res); 
+		
+		});
+	},  
+};
+
+
+
+
+module.exports = burger;
+
+
+
+
+
+
+
+
+
+
